273e67bf1830993d8748d773d56e2cae6a305c39
AICHA LAHNITE <halaicha300@gmail.com>
2023-11-29 20:05:30 +0100

    ggg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Installs flask
 
-package { 'pip3':
-  ensure   => '2.1.0',
-  provider => 'flask',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-flask':
+  ensure   => installed,
+  require  => Package['python3-pip'],
+  provider => 'yum',
 }
