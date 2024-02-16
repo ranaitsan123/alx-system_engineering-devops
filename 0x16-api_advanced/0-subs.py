753faa3dc0d19ab6b146e2eb373bb26d7702641a
AICHA LAHNITE <halaicha300@gmail.com>
2024-02-16 11:20:50 +0100

    dg

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,15 +6,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Custom User Agent"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
-
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    count = number_of_subscribers(subreddit)
-    print("Number of subscribers:", count)
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
