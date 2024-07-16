577f7dba53bbbbad95ae4e34ab8dbac355622132
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-07-16 21:00:44 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# Installs flask
+# This Puppet manifest installs Flask version 2.1.0 and Werkzeug version 2.0.3 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install the required versions of Flask and Werkzeug
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep 2.1.0 && /usr/bin/pip3 show Werkzeug | grep 2.0.3',
+  require => Package['python3-pip'],
 }
